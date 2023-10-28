8018d71d9484705938b002547f0a27ced5a14659
Restyled.io <commits@restyled.io>
2023-10-28 10:06:28 +0000

    Restyled by black

--- export.py
+++ export.py
@@ -4,19 +4,19 @@ import os
 import zipfile
 import shutil
 
-url = 'https://lb.dioco.io/base_items_itemsCSVExport_7'
+url = "https://lb.dioco.io/base_items_itemsCSVExport_7"
 
 headers = {
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/json',
-    'Origin': 'https://www.languagereactor.com',
-    'Referer': 'https://www.languagereactor.com/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'cross-site',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320'
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6",
+    "Connection": "keep-alive",
+    "Content-Type": "application/json",
+    "Origin": "https://www.languagereactor.com",
+    "Referer": "https://www.languagereactor.com/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "User-Agent": "Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320",
 }
 
 data = {
@@ -31,7 +31,7 @@ data = {
     "exportMedia": False,
     "itemsSinceLastExportOnly": False,
     "userEmail": "mediandrey@gmail.com",
-    "diocoToken": "UwR6l31fOW37nN8OdjjQeer7vMZktx7S6I2SwhZ7wODwY3ASDjSo6r0hI88P8TwGfHljykePmO4TJmGCmCMhNw=="
+    "diocoToken": "UwR6l31fOW37nN8OdjjQeer7vMZktx7S6I2SwhZ7wODwY3ASDjSo6r0hI88P8TwGfHljykePmO4TJmGCmCMhNw==",
 }
 
 # def get_data_url() -> str:
@@ -48,19 +48,18 @@ def download_csv_lr(file_url: str) -> None:
     local_filename = "lr.zip"
     response = requests.get(file_url, stream=True)
     if response.status_code == 200:
-        with open(local_filename, 'wb') as file:
+        with open(local_filename, "wb") as file:
             for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
                     file.write(chunk)
         print(f"File '{local_filename}' has been downloaded.")
     else:
-        print(
-            f"Failed to download the file. Status code: {response.status_code}")
+        print(f"Failed to download the file. Status code: {response.status_code}")
     return local_filename
 
 
 def unzip(filename: str) -> None:
-    with zipfile.ZipFile(filename, 'r') as zip_ref:
+    with zipfile.ZipFile(filename, "r") as zip_ref:
         zip_ref.extractall("unziplr")
     try:
         os.remove(filename)
@@ -81,10 +80,11 @@ def unzip(filename: str) -> None:
 
 def prepare_to_import(input_file: str, output_file: str) -> None:
     try:
-        df = pd.read_csv(input_file, delimiter='\t')
-        df['Combined_Column'] = df.apply(
-            lambda row: f"{row.iloc[4]} [{row.iloc[5]}] ({row.iloc[6]})", axis=1)
-        df = df[['Combined_Column', df.columns[8]]]
+        df = pd.read_csv(input_file, delimiter="\t")
+        df["Combined_Column"] = df.apply(
+            lambda row: f"{row.iloc[4]} [{row.iloc[5]}] ({row.iloc[6]})", axis=1
+        )
+        df = df[["Combined_Column", df.columns[8]]]
         df.to_csv(output_file, index=False)
     except Exception as e:
         print(f"n error occurred: {str(e)}")
@@ -96,5 +96,5 @@ def prepare_to_import(input_file: str, output_file: str) -> None:
 
 
 if __name__ == "__main__":
-    unzip(download_csv_lr(''))
+    unzip(download_csv_lr(""))
     prepare_to_import("items.csv", "out.csv")
